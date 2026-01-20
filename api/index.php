f32bf7360a3b76ab46b2bc05be930960ab87d0c4
bouchra baifouch <bouchrabaidouch@gmail.com>
2026-01-20 11:30:22 +0100

    Fix Vercel deployment and update styles

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+
+    $request = Illuminate\Http\Request::capture()
+
+);
+
+echo $response->getContent();
+
+$kernel->terminate($request, $response);
